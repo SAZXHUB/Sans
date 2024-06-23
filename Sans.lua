efda7a1466098c4ebbaba97f2a920beef43861fe
SAZXHUB <130627354+SAZXHUB@users.noreply.github.com>
2024-06-24 05:49:46 +0700

    Update and rename README.md to Sans.lua

---
+++ Sans.lua
@@ -0,0 +1,2 @@
+warn("slime.lua has been moved to my cdn, please use 'https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/FESans.lua' instead.")
+loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/FESans.lua"))()
